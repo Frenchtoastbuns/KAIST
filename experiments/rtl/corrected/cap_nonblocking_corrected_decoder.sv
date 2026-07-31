1961fb2e9c860da0c1eb8bdbe7a8f1d24d7f92be
Frenchtoastbuns <aarongeorge243@gmail.com>
2026-08-01 02:40:31 +0900

    Wire compact left-DP write body into corrected core

--- experiments/rtl/corrected/cap_nonblocking_corrected_decoder.sv
+++ experiments/rtl/corrected/cap_nonblocking_corrected_decoder.sv
@@ -1,6 +1,7 @@
 `include "cap_nb_part_00.svh"
 `include "cap_nb_part_01.svh"
 `include "cap_nb_part_02.svh"
+`include "cap_nb_part_02b.svh"
 `include "cap_nb_part_03.svh"
 `include "cap_nb_part_04.svh"
 `include "cap_nb_part_05.svh"
