eea5d03349c20ff92e55a88da9a724563a3d3a24
Frenchtoastbuns <aarongeorge243@gmail.com>
2026-08-01 16:49:15 +0900

    Add canonical trace exporter entry point

---
+++ experiments/rtl/corrected/canonical_reference/export_canonical_trace.cc
@@ -0,0 +1,4 @@
+#include "export_canonical_trace_part_00.inc"
+#include "export_canonical_trace_part_01.inc"
+#include "export_canonical_trace_part_02.inc"
+#include "export_canonical_trace_part_03.inc"
